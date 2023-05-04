87dbe8d825c336840e9f8236791c14da8ab33877
Boyar <you@example.com>
2023-05-04 18:47:49 +0300

    Add first iteration of parser

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+url = "https://domclick.ru/arenda/kvartiry"
+
+doc = requests.get(url)
+
+print(doc.text)
